021470291671324dde81b55562608a67acb938df
rhithesh <rhithesh1947@gmail.com>
2024-12-23 23:45:22 +0530

    added helmut and ratelimiting

--- backend/controller/registration.controller.js
+++ backend/controller/registration.controller.js
@@ -3,8 +3,8 @@ import Event from "../models/event.model.js";
 
 export const createRegistration = async (req, res) => {
     try {
-        const { name, phoneNumber, email, hostelName, eventTitle } = req.body;
-        if (!name || !phoneNumber || !email || !hostelName || !eventTitle) {
+        const { name, phoneNumber, email, hostelName, eventTitle,group } = req.body;
+        if (!name || !phoneNumber || !email || !hostelName || !eventTitle || !group) {
             return res.status(400).json({ message: "All fields are required." });
         }
 
@@ -27,6 +27,7 @@ export const createRegistration = async (req, res) => {
             email,
             hostelName,
             eventId: event._id,
+            group: group
         });
 
         const savedRegistration = await newRegistration.save();
--- backend/index.js
+++ backend/index.js
@@ -3,14 +3,26 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
-import eventRoute from "./routes/event.route.js";
+import eventCreateRoute from "./routes/event.route.js";
 import registrationRouter from "./routes/registration.route.js";
 import bodyParser from "body-parser";
 import multer from "multer";
+import helmet from "helmet";
+import rateLimit from 'express-rate-limit';
 
 dotenv.config({});
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet({
+  // Custom helmet configuration here
+}));
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 1 hour in milliseconds
+  max: 100, // Limit each IP to 100 requests per hour
+  message: 'Too many requests, please try again later.',
+});
+app.use(limiter);
+
 
 // Middleware
 app.use(express.json());
@@ -25,10 +37,10 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT || 3000;
 
 // API Routes
-app.use("/api/v1/event", eventRoute);
+app.use("/api/v1/event", eventCreateRoute);
 app.use("/api/v1/registration", registrationRouter);
 app.get("/", (req, res) => {
-  return res.json({ "Hello": "HI" });
+  return res.json({ "All Routes are running": "Welcome to farushe" });
 });
 
 app.post("/",(req,res)=>{
