c7922d03d727e0f2bc6d932fab2b27e038c72979
happines <rhithesh1947@gmail.com>
2025-03-11 09:59:39 +0530

    Update constants.js

--- adminfrontend/src/app/utils/constants.js
+++ adminfrontend/src/app/utils/constants.js
@@ -1,3 +1,3 @@
-export const EVENT_API_END_POINT = "http://13.60.17.160:4000/api/v1/event"
-export const REGISTRATION_API_END_POINT = "http://13.60.17.160:4000/api/v1/registration"
-export const RESULTS_END_POINT = "http://13.60.17.160:4000/api/v1/results"
+export const EVENT_API_END_POINT = "https://farouche25.tech/api/v1/event"
+export const REGISTRATION_API_END_POINT = "https://farouche25.tech/api/v1/registration"
+export const RESULTS_END_POINT = "https://farouche25.tech/api/v1/results"
